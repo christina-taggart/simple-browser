d209dadae427ec9651d42bd2fb883c05fc4ed314
Patrick Vilhena & Eli Shkurkin <pair+nifengle.shkurkin@devbootcamp.com>
2014-01-15 17:08:19 -0800

    Release 0: IE 6

--- source/browser.rb
+++ source/browser.rb
@@ -4,17 +4,25 @@ require 'nokogiri'
 require_relative 'util'
 
 class Browser
+
   def run!
+    puts "Welcome to Internet Explorer 6!\n"
+    puts "The web as you first experienced it. Over dialup. In the 90s."
+    print "url> "
+    Page.new(gets.chomp)
     # Run the browser
     # Display a prompt for a user
     # Parse their input
     # Display useful results by instantiating
     #   a new Page and calling methods on it.
-    
+
     # Questions:
     #  1. How can a user quit the browser gracefully?
     #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
   end
 end
+
+# page = Page.new('http://www.google.com')
+# p page.noko_object
 
 Browser.new.run!
--- source/util.rb
+++ source/util.rb
@@ -1,19 +1,63 @@
+
+
 class Page
+  attr_reader :url, :noko_object, :links
+
   def initialize(url)
+    puts "Fetching..."
+    @url = URI.parse(url)
+    @noko_object = convert_to_noko
+    @links = get_links
+    run
   end
-  
+
+  def run
+    print_title
+    get_content_length
+    print_links
+  end
+
   def fetch!
+    Net::HTTP.get_response(url)
   end
-  
+
+  def convert_to_noko
+    Nokogiri::HTML(fetch!.body)
+  end
+
   def title
+    noko_object.search("title").inner_text
   end
-  
-  def links
+
+  def print_title
+    puts "Title: #{title}"
+  end
+
+  def get_links
+    fetch_links
+    clean_links
     # Research alert!
     # How do you use Nokogiri to extract all the link URLs on a page?
     #
     # These should only be URLs that look like
     #   <a href="http://somesite.com/page.html">Click here!</a>
     # This would pull out "http://somesite.com/page.html"
+  end
+
+  def fetch_links
+    @links = noko_object.search("a").map{ |link| link['href']}
+  end
+
+  def clean_links
+    @links.select! { |link| link if link && link.match(/\//) }
+  end
+
+  def print_links
+    puts "Links:"
+    @links.each{ |link| puts "  #{link}" }
+  end
+
+  def get_content_length
+    puts "Content length: #{@noko_object.search('body').inner_text.scan(/[a-z]+|[0-9]+/i).join.length} characters"
   end
 end
