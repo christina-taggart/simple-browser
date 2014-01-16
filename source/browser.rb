93bc307f69a940544008f30e73fea076e36caf30
Apprentice <apprentice@dbc28.local>
2014-01-15 17:18:32 -0800

    Completed basic browser

--- source/browser.rb
+++ source/browser.rb
@@ -1,20 +1,76 @@
-require 'net/http'
-require 'nokogiri'
 
 require_relative 'util'
 
 class Browser
+  include Display
+  def initialize
+    @current_page = Page.new()
+    @exit_flag = true
+  end
   def run!
-    # Run the browser
-    # Display a prompt for a user
-    # Parse their input
-    # Display useful results by instantiating
-    #   a new Page and calling methods on it.
-    
-    # Questions:
-    #  1. How can a user quit the browser gracefully?
-    #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
+    welcome
+    run_user_session
+    goodbye
+  end
+
+  def run_user_session
+    while @exit_flag
+      print "url> "
+        get_current_page
+        run_user_input
+    end
+  end
+
+  def get_current_page
+    input = gets.chomp
+    if input == 'q'
+      @exit_flag = false
+      return
+    end
+    @current_page = Page.new("http://#{input}")
+  end
+
+  def run_user_input
+    while true && @exit_flag
+      options_menu
+      input = gets.chomp.downcase
+      case input
+      when 't'
+        display_page_title
+      when 'l'
+        display_page_links
+      when 'h'
+        display_page_html
+      when 'b'
+        break
+      else
+        puts "Not valid input"
+      end
+    end
+  end
+
+  def display_page_title
+    puts "Title: #{@current_page.title}\n"
+  end
+
+  def display_page_links
+    puts "Links: \n #{@current_page.links}\n"
+  end
+
+  def display_page_html
+    puts "HTML:\n#{@current_page.html}\n"
   end
 end
 
 Browser.new.run!
+
+
+# Run the browser
+    # Display a prompt for a user
+    # Parse their input
+    # Display useful results by instantiating
+    #   a new Page and calling methods on it.
+
+    # Questions:
+    #  1. How can a user quit the browser gracefully?
+    #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
--- source/util.rb
+++ source/util.rb
@@ -1,19 +1,54 @@
+require 'net/http'
+require 'nokogiri'
+
 class Page
-  def initialize(url)
+  attr_accessor :title, :links, :html, :nok_obj
+  def initialize(url = "http://www.alexleishman.com")
+    @url = url
+    @html = ""
+    fetch!
   end
-  
+
   def fetch!
+    @html = Net::HTTP.get(URI(@url))
+    @nok_obj = Nokogiri::HTML(@html)
   end
-  
+
   def title
+    @nok_obj.css('title').text
   end
-  
+
   def links
-    # Research alert!
-    # How do you use Nokogiri to extract all the link URLs on a page?
-    #
-    # These should only be URLs that look like
-    #   <a href="http://somesite.com/page.html">Click here!</a>
-    # This would pull out "http://somesite.com/page.html"
+    all_links = @nok_obj.css('a').map { |obj| obj['href'] }
+    relative_links = all_links.select { |link| link =~ /^\// }
+  end
+
+end
+
+module Display
+  def options_menu
+    puts <<-eos
+    Options       Description
+      l      |     Show links on page
+      t      |     Show page title
+      h      |     Show page html
+      b      |     Go back
+    eos
+  end
+
+  def welcome
+    puts <<-eos
+    Welcome to the world's best web browser. This browser is much better than Chrome, Firefox and IE combined.
+    Use at your own risk, don't be overwhelmed by the awesomeness!!!
+
+    Created by Leishman Industries, Inc.
+    Copyright 2014
+    eos
+  end
+
+  def goodbye
+    puts <<-eos
+    Thank you for using our browser. I hope you enjoyed it :)
+    eos
   end
 end
