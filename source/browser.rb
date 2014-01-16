25288872a6fc5c5eb6e542e17bbfcd13a24418dc
Apprentice <apprentice@dbc04.local>
2014-01-15 19:46:52 -0800

    Release 1

--- source/browser.rb
+++ source/browser.rb
@@ -1,20 +1,31 @@
 require 'net/http'
 require 'nokogiri'
-
+require "uri"
 require_relative 'util'
 
 class Browser
   def run!
-    # Run the browser
-    # Display a prompt for a user
-    # Parse their input
-    # Display useful results by instantiating
-    #   a new Page and calling methods on it.
-    
-    # Questions:
-    #  1. How can a user quit the browser gracefully?
-    #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
+    puts "Please enter a URL"
+    url = gets.chomp
+    doc = Nokogiri::HTML(open(url))
+    page = Page.new(url, doc)
+
+    # Actually makes the HTTP request
+    page.fetch!
+
+    # Returns the URL of the underlying page
+    puts "------"
+    puts "You are visiting #{page.url}"
+    puts "Title: #{page.title}"
+
+    # Returns an Array of URLs (as Strings).
+    puts "------"
+    p page.display_links
   end
 end
 
 Browser.new.run!
+
+
+# http://www.cnn.com/2013/02/06/travel/private-jets/index.html
+
--- source/util.rb
+++ source/util.rb
@@ -1,19 +1,55 @@
+require "net/http"
+require "uri"
+require 'nokogiri'
+require 'open-uri'
+
+
 class Page
-  def initialize(url)
+
+  attr_accessor :url
+  attr_reader :title
+
+  def initialize(url, doc)
+    @url = url
+    @doc = doc
+    @uri = URI.parse(@url)
+    @link_list = gather_links
+    @title = get_title[0]
   end
-  
+
   def fetch!
+    @http = Net::HTTP.new(@uri.host, @uri.port)
+    @response = @http.request(Net::HTTP::Get.new(@uri.request_uri))
+    p @response
   end
-  
-  def title
+
+  def get_title
+    @doc.search('.cnn_storyarea h1').map do |element|
+      element.inner_text
+    end
   end
-  
-  def links
-    # Research alert!
-    # How do you use Nokogiri to extract all the link URLs on a page?
-    #
-    # These should only be URLs that look like
-    #   <a href="http://somesite.com/page.html">Click here!</a>
-    # This would pull out "http://somesite.com/page.html"
+
+  def display_title
+    puts @title[0]
+  end
+
+  def gather_links
+    @doc.search('.cnn_storypgraphtxt > a').map do |element|
+      element['href']
+    end
+  end
+
+  def display_links
+    puts "This page contains the following links:"
+    @link_list.each {|link| puts link}
   end
 end
+
+
+#---------------------------------------------------------------------------
+
+
+
+
+
+
