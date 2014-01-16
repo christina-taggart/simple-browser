1f0a523348152e451648b9a3219767677abc1e6c
Elliot Chalfant <epchalfant@gmail.com>
2014-01-15 19:14:15 -0800

    Release 1: dumbest browser ever

--- source/browser.rb
+++ source/browser.rb
@@ -5,15 +5,13 @@ require_relative 'util'
 
 class Browser
   def run!
-    # Run the browser
-    # Display a prompt for a user
-    # Parse their input
-    # Display useful results by instantiating
-    #   a new Page and calling methods on it.
-    
-    # Questions:
-    #  1. How can a user quit the browser gracefully?
-    #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
+    input = ""
+    p "Enter your URL to browse. Type 'exit' to close"
+    while input != "exit"
+      print "url> "
+      input = gets.chomp
+      Page.new(input)
+    end
   end
 end
 
